b1b4e03c116fc884c5ce68b6a123f6341aa03caa
moctar <niangmoctar172@gmail.com>
2026-03-18 03:17:38 +0000

    Aujourd'hui, j'ai trvailé sur la partie fornt-end de la premiere exercice

--- exo.js
+++ exo.js
@@ -9,9 +9,9 @@ formulair.addEventListener("submit", function(event){
     event.preventDefault();
 
     //maintenant on recupere les valeur saisi du formulaire
-    let nom = document.getElementById("nom").value;
-    let prenom = document.getElementById("prenom").value;
-    let adresse = document.getElementById("adresse").value;
+    let nom = document.getElementById("nom").value.trim();
+    let prenom = document.getElementById("prenom").value.trim();
+    let adresse = document.getElementById("adresse").value.trim();
 
     if(!nom || !prenom || !adresse){
         alert("remplir tous les champs")
@@ -19,6 +19,7 @@ formulair.addEventListener("submit", function(event){
 
     //on ajoute les valeurs saisis sur le tableau etudiant !!
     etudiant.push({nom:nom, prenom:prenom, adresse:adresse});
+    alert("ajoutement avec succee");
 
     afficher()
     document.getElementById("nom").value="";
@@ -31,11 +32,26 @@ formulair.addEventListener("submit", function(event){
 //creer la fonction qui va nous permettre parcourire la table ou le tableau d'objet
 function afficher(){
    const resultat = etudiant.map(function(personne,index){
-      return "<li>" +
-                 "👤 " + personne.nom + " " + personne.prenom +
-                 " | 📧 " + personne.adresse +
-                 "<button onclick='supprimer("+ index +")'>supprimer</button>"
-               "</li>";
+      return ` 
+         <tr>
+            <td class="">${index + 1}</td>
+            <td>
+              <div class="text-2xl">
+               <span><i class="fa-solid fa-circle-user"></i></span>
+                <span class="nom-text">${personne.nom}</span>
+                
+              </div>
+            </td>
+            <td>${personne.prenom}</td>
+            <td><span><i class="fa-solid fa-map-pin"></i> ${personne.adresse}</span></td>
+            <td>
+               <i class="fa-solid fa-basket-shopping cursor-pointer"></i> <button onclick="supprimer(${index})" class="bg-red-500">
+               suprimer
+              </button>
+            </td>
+          </tr>
+        `;
+      
               
    })
     document.getElementById("liste").innerHTML = resultat.join("")
@@ -50,3 +66,27 @@ function afficher(){
  // console.log("votre nom est:" + " " + nom );
     // console.log("votre prenom est:" + " " + prenom );
     // console.log("votre adresse est:" + " " + adresse );
+    // <li class="bg-white rounded-xl shadow p-4 flex justify-between items-center hover:bg-gray-50 transition"> +
+    //              "👤 " + personne.nom + " " + personne.prenom +
+    //              " | 📧 " + personne.adresse +
+    //              "<button onclick='supprimer("+ index +")'>supprimer</button>"
+    //            </li>
+
+//      <thead>
+//     <tr>
+//       <th>nom</th>
+//       <th>prenom</th>
+//       <th>adresse</th>
+//       <th>Action</th>
+//     </tr>
+//   </thead>
+//   <tbody>
+//     <tr>
+//       <td>${personne.nom}</td>
+//       <td>${personne.prenom}</td>
+//       <td>${personne.adresse}</td>
+//       <td><button onclick="supprimer(${index})"></button></td>
+//     </tr>
+//   </tbody>
+// </table>
+ // <div class="avatar">${initiale}</div>
